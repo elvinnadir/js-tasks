09174f5d1f14afddbee3e8d39dc2e8d347de4151
elvinnadir <elvinnadir92@gmail.com>
2022-08-09 19:17:10 +0400

    js practice

---
+++ main.js
@@ -0,0 +1,18 @@
+// task1  print hello world
+// console.log("Hello World");
+
+// task2 return square a number
+// console.log(Math.sqrt(16));
+
+
+// task3 reverse number
+// function reversedNum(num) {
+//     return (
+//         num.toString()
+//         .split('')
+//         .reverse()
+//         .join('')
+//     )                 
+//   }
+//   console.log(reversedNum(1345))
+
