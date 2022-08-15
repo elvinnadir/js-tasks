1c9318350445e0cb388e1b111ce92e9f96ae768c
elvinnadir <elvinnadir92@gmail.com>
2022-08-15 16:21:42 +0400

    new tasks

---
+++ form.js
@@ -0,0 +1,14 @@
+let form = document.getElementById("form");
+let action = document.getElementsByClassName("btn");
+let name = document.getElementsByClassName("name");
+let address = document.getElementsByClassName("address");
+let selectCity = document.getElementsByClassName("cities");
+let hobby = document.getElementsByClassName("hobby");
+let hiddenBox = document.getElementsByClassName("hidden");
+let myInput;
+
+
+function hobbyBtn() {
+console.log("click here");
+}
+action.addEventListener('click',hobbyBtn());
